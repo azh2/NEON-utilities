a2ad57ec2eaf5bd4a5494480ca27b77b61befde4
chrlaney <claney@battelleecology.org>
2018-03-05 16:50:11 -0700

    This seems to work, at least once in midstream.

--- neonDataStackR/R/byFileAOP_test.R
+++ neonDataStackR/R/byFileAOP_test.R
@@ -3,6 +3,7 @@
 
 #' @author
 #' Claire Lunch \email{clunch@battelleecology.org}
+#' Christine Laney \email{claney@battelleecology.org}
 
 #' @description
 #' Query the API for AOP data by site, year, and product, and download all files found. Downloads serially to avoid overload; may take a very long time.
@@ -22,6 +23,8 @@
 # changelog and author contributions / copyrights
 #   Claire Lunch (2018-02-19)
 #     original creation
+#   Christine Laney (2018-03-05)
+#     Added functionality to get new list of URLs if the old ones expire, during the download stream.
 ##############################################################################################
 
 byFileAOP <- function(dpID, site="SJER", year="2017", check.size=TRUE) {
@@ -55,12 +58,12 @@ byFileAOP <- function(dpID, site="SJER", year="2017", check.size=TRUE) {
     stop("There are no data at the selected site and year.")
   }
 
-  # get all the file names, and stash the URLs in an object
+  # get and stash the file names, S3 URLs, file size, and download status (default = 0) in a data frame
   getFileUrls <- function(m.urls){
     file.urls <- c(NA, NA, NA)
-    messages <- NA
-    for(i in 1:length(month.urls)) {
-      tmp <- httr::GET(month.urls[i])
+    messages <- character()
+    for(i in 1:length(m.urls)) {
+      tmp <- httr::GET(m.urls[i])
       tmp.files <- jsonlite::fromJSON(httr::content(tmp, as="text"),
                                     simplifyDataFrame=T, flatten=T)
 
@@ -83,12 +86,13 @@ byFileAOP <- function(dpID, site="SJER", year="2017", check.size=TRUE) {
       file.urls$downloaded <- 0
 
       if(length(messages) > 0){writeLines(messages)}
+      file.urls <- file.urls[-1, ]
       return(file.urls)
     }
   }
 
   file.urls.current <- getFileUrls(month.urls)
-  downld.size <- sum(as.numeric(as.character(file.urls.current$size)), na.rm=T)/1e6
+  downld.size <- sum(as.numeric(as.character(file.urls.current$size)), na.rm=T)
   downld.size.read <- humanReadable(downld.size, units = "auto", standard = "SI")
 
   # ask user if they want to proceed
@@ -102,38 +106,27 @@ byFileAOP <- function(dpID, site="SJER", year="2017", check.size=TRUE) {
   }
 
   # create folder in working directory to put files in
-  filepath <- paste(getwd(), "/files", substr(dpID, 5, 9), sep="")
+  filepath <- paste(getwd(), "/", dpID, sep="")
   dir.create(filepath)
 
   # copy zip files into folder
-
-  for(i in 2:nrow(file.urls.current)) {
-    tryCatch(
-      {downloader::download(file.urls.current$URL[i], paste(filepath, file.urls$name[i], sep="/"), mode="wb")
-        messages <- c(messages, paste(nrow(file.urls)-1, "files downloaded to",
-                                      filepath, sep=" "))
-        writeLines(paste0(messages[-1], collapse = "\n"))},
-      error=function(err) {
-        "File could not be downloaded. URLs may have expired. Getting new URLs."
-        file.urls.new <- getFileUrls(month.urls)
-        file.urls.current <- merge(file.urls.new, file.urls.current, by = names(file.urls.new), all = T)
-        writeLines("Continuing downloads.")
-        i = 1
-        downloader::download(file.urls.current$URL[i], paste(filepath, file.urls$name[i], sep="/"), mode="wb")
-        messages <- c(messages, paste(nrow(file.urls)-1, "files downloaded to",
-                                      filepath, sep=" "))
-        writeLines(paste0(messages[-1], collapse = "\n"))
-        }
-    )
+  j <- 1
+  while(j <= nrow(file.urls.current)) {
+    t <- try(downloader::download(file.urls.current$URL[j], paste(filepath, file.urls.current$name[j], sep="/"), mode="wb"), silent = T)
+    if(class(t) == "try-error"){
+      writeLines("File could not be downloaded. URLs may have expired. Getting new URLs.")
+      file.urls.new <- getFileUrls(month.urls)
+      file.urls.current <- merge(file.urls.current, file.urls.new, by = names(file.urls.current), all.x = F, all.y = T)
+      writeLines("Continuing downloads.")}
+    else {
+      messages <- c(messages, paste(file.urls.current$name[j], "downloaded to",
+                                    filepath, sep=" "))
+      j = j + 1
+    }
   }
-}
 
-y <- numeric()
-for(i in 1:length(x)){
-  t <- try(y <- c(y, 2*x[[i]]))
-  if (class(t) == "try-error"){
-    writeLines("You can't do that!")
-    x[[i]] <- 0
-    i <- i-1
-  }
+  messages <- c(messages, paste(nrow(file.urls.current)-1, "files downloaded to",
+                                filepath, sep=" "))
+  writeLines(paste0(messages[-1], collapse = "\n"))
+
 }
