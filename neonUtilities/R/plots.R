d31057268e7e9e9d7cf806b97df4d221c60546db
bw4sz <benweinstein2010@gmail.com>
2018-09-04 14:38:06 -0700

    changing to data structure

---
+++ neonUtilities/R/plots.R
@@ -0,0 +1,8 @@
+#' Spatial Location of NEON plots
+#'
+#' All_Neon_TOS_Polygons_V5.shp
+#'
+#' @format A sf object with 3756 rows and 37 variables:
+#' 
+#' @source \url{http://data.neonscience.org/data-product-view?dpCode=DP1.10098.001}
+"plots"
